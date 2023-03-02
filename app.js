6f0c6102bf2a2bbe43cd744c378919e7169d13a1
Mohammedareebali <mohammedareebali740@gmail.com>
2023-03-02 00:15:23 +0000

    Part II - API GET Routes: Return Unsplash Photos

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
-
+const photoRoutes = require('./routes/photoRoutes');
 app.use(express.json());
-
+app.use('/api/photos', photoRoutes);
 app.get('/', (req, res) => {
   res.status(200).json({ message: "Welcome to the Unsplash API!" });
 });
---
+++ routes/photoRoutes.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const express = require('express');
+const router = express.Router();
+require('dotenv').config({
+    path:'./dev.env'
+})
+const unsplashBaseUrl = 'https://api.unsplash.com';
+const unsplashAccessKey = process.env.ACCESS_KEY; 
+
+// Route to get an array of raw Unsplash photo URLs
+router.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(`${unsplashBaseUrl}/photos`, {
+      headers: {
+        Authorization: `Client-ID ${unsplashAccessKey}`,
+      },
+    });
+    const photoUrls = response.data.map((photo) => photo.urls.raw);
+    res.status(200).json(photoUrls);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error. Please try again later." });
+  }
+});
+
+// Route to get a single photo object from the Unsplash API based on a photo's id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await axios.get(`${unsplashBaseUrl}/photos/${id}`, {
+      headers: {
+        Authorization: `Client-ID ${unsplashAccessKey}`,
+      },
+    });
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error. Please try again later." });
+  }
+});
+
+module.exports = router;
