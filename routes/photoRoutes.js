fc3c9689d0fbaaddc473340452876f7ea70d5fec
Mohammedareebali <mohammedareebali740@gmail.com>
2023-03-02 00:24:17 +0000

    Part III - Advanced Routes: Return Combined Data

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 require('dotenv').config({
     path:'./dev.env'
 })
+
 const unsplashBaseUrl = 'https://api.unsplash.com';
 const unsplashAccessKey = process.env.ACCESS_KEY; 
 
@@ -38,5 +39,28 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({ message: "Server error. Please try again later." });
   }
 });
-
+// Route to get a user's Unsplash photos based on a username
+router.get('/user/:username', async (req, res) => {
+    const { username } = req.params;
+    try {
+      const response = await axios.get(`${unsplashBaseUrl}/users/${username}/photos`, {
+        headers: {
+          Authorization: `Client-ID ${unsplashAccessKey}`,
+        },
+      });
+      const photos = response.data.map((photo) => {
+        return {
+          id: photo.id,
+          username: photo.user.username,
+          description: photo.description ? photo.description : "No description provided.",
+          url: photo.urls.raw,
+        };
+      });
+      res.status(200).json(photos);
+    } catch (error) {
+      console.error(error);
+      res.status(error.response.status).json({ message: error.response.data });
+    }
+  });
+  
 module.exports = router;
